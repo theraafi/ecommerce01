e3ba434783db0974809da1c5098f9bd350966ad9
fahimraafi <fahim.raafi.dev@gmail.com>
2023-09-06 11:23:38 +0600

    Class #30

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\FrontendController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/profile', [App\Http\Controllers\HomeController::class, 'profile'])->name('profile');
+
